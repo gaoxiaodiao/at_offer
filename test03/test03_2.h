985bbdc19711027311690d05aecad689dee04267
gaoxiaodiao <805888196@qq.com>
2017-03-16 14:52:40 +0800

    面试题03:二维数组的查找

---
+++ test03/test03_2.h
@@ -0,0 +1,59 @@
+/*
+*面试题02:二维数组的查找
+*作者:高小调
+*日期:2017-03-16
+*集成开发环境:Microsoft Visual Studio 2010
+*/
+#include<cassert>
+bool YangMatrixFind(int *arr,size_t row,size_t col,int key){
+	assert(arr);
+	//起始参考位置为右上角
+	int x = 0;
+	int y = col-1;
+	while(x<row && y>=0){
+		if(arr[x*col+y]<key){
+			++x;	//当前位置小于key值,剔除一列
+		}else if(arr[x*col+y]>key){
+			--y;	//当前位置大于key值,剔除一行
+		}else{
+			return true;	//找到返回真
+		}
+	}
+	return false;		//未找到返回假
+}
+
+void TestYangMatirx(){
+	int arr1[4][4]={{1,2,8,9}
+					,{2,4,9,12}
+					,{4,7,10,13}
+					,{6,8,11,15}};
+	int arr2[4][3] = {{1,2,8}
+					,{2,4,9}
+					,{4,7,10}
+					,{6,8,11}};
+	int arr3[3][4] = {{1,2,8,9}
+					,{2,4,9,12}
+					,{4,7,10,13}};
+
+	std::cout<<YangMatrixFind((int*)arr1,4,4,1)<<std::endl;				//找最小值
+	std::cout<<YangMatrixFind((int*)arr1,4,4,15)<<std::endl;			//找最大值
+	std::cout<<YangMatrixFind((int*)arr1,4,4,9)<<std::endl;				//找介于最小值与最大值之间
+	std::cout<<YangMatrixFind((int*)arr1,4,4,-1)<<std::endl;			//不存在的最小值
+	std::cout<<YangMatrixFind((int*)arr1,4,4,5)<<std::endl;				//不存在介于最小值与最大值之间
+	std::cout<<YangMatrixFind((int*)arr1,4,4,16)<<std::endl<<std::endl; //不存在的最大值
+
+	std::cout<<YangMatrixFind((int*)arr2,4,3,1)<<std::endl;				 //找最小值
+	std::cout<<YangMatrixFind((int*)arr2,4,3,11)<<std::endl;			 //找最大值
+	std::cout<<YangMatrixFind((int*)arr2,4,3,7)<<std::endl;				 //找介于最小值与最大值之间
+	std::cout<<YangMatrixFind((int*)arr2,4,3,-1)<<std::endl;			 //不存在的最小值
+	std::cout<<YangMatrixFind((int*)arr1,4,4,5)<<std::endl;				//不存在介于最小值与最大值之间
+	std::cout<<YangMatrixFind((int*)arr2,4,3,16)<<std::endl<<std::endl;	//不存在的最大值
+
+	std::cout<<YangMatrixFind((int*)arr3,3,4,1)<<std::endl;				//找最小值
+	std::cout<<YangMatrixFind((int*)arr3,3,4,13)<<std::endl;			//找最大值
+	std::cout<<YangMatrixFind((int*)arr3,3,4,9)<<std::endl;				//找介于最小值与最大值之间
+	std::cout<<YangMatrixFind((int*)arr3,3,4,-1)<<std::endl;			//不存在的最小值
+	std::cout<<YangMatrixFind((int*)arr1,4,4,5)<<std::endl;				//不存在介于最小值与最大值之间
+	std::cout<<YangMatrixFind((int*)arr3,3,4,16)<<std::endl<<std::endl; //不存在的最大值
+
+}
